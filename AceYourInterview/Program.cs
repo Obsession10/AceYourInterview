4c1a5679939cec88b592c9900f748d05a0da314d
Obsession10 <gemeniucadrian1998@gmail.com>
2020-06-26 19:18:16 +0300

    initial

--- AceYourInterview/Program.cs
+++ AceYourInterview/Program.cs
@@ -19,7 +19,14 @@ namespace AceYourInterview
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseStartup<Startup>()
-                .Build();
+                .Build()
+                .Run();
+
+                
+        
+        
     }
 }
